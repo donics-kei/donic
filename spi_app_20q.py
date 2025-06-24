a1df6a4638022ac9731c47531dbf2922c0cfbaf0
donics-kei <donai@n-nics.com>
2025-06-24 09:32:04 +0900

    Update spi_app_20q.py

--- spi_app_20q.py
+++ spi_app_20q.py
@@ -50,6 +50,9 @@ if q_index < NUM_QUESTIONS:
     else:
         selected = None
         if st.session_state.get("selected_choice"):
+            labels = ['a', 'b', 'c', 'd', 'e']
+            choices = [str(q['choice1']), str(q['choice2']), str(q['choice3']), str(q['choice4']), str(q['choice5'])]
+            labeled_choices = [f"{l}. {c}" for l, c in zip(labels, choices)]
             selected_index = labeled_choices.index(st.session_state["selected_choice"])
             your_answer = labels[selected_index]
             correct_answer = str(q['answer']).lower().strip()
