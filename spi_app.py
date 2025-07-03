c11303c8af6461a378f4069044f706e23eacaa7f
donics-kei <donai@n-nics.com>
2025-07-03 09:43:00 +0900

    Update spi_app.py

--- spi_app.py
+++ spi_app.py
@@ -182,5 +182,10 @@ if st.session_state.page == "result":
             score += 1
 
     st.success(f"🎯 最終スコア：{score} / {st.session_state.num_questions}")
-    if st.button("もう一度解く"):
+      if st.button("もう一度解く"):
+        for k in list(st.session_state.keys()):
+            if k not in ["authenticated"]:
+                del st.session_state[k]
+        st.rerun()
+        st.stop()
 
